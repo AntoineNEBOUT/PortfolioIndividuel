56a08347f255db6904511ae3d793ce0914c08c7b
Antoine NEBOUT <Antoine.NEBOUT@protonmail.com>
2024-10-23 17:03:08 +0200

    Upd:index.html

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,6 +1,56 @@
 const themeToggleBtn = document.getElementById('theme_toggle');
+// const bodyIndex = document.getElementById('bodyIndex');
 const body = document.body;
+let isDark = false;
 
-themeToggleBtn.addEventListener('click', () => {
-    body.classList.toggle('dark');
-});
+// window.addEventListener('load', applyTheme);
+
+themeToggleBtn.addEventListener('click', changeTheme);
+
+function changeTheme() {
+    if (isDark) {
+        isDark = false;
+        setCookie('darkMode', 'false', 7);
+        // console.log(document.cookie);
+        body.classList.remove('dark');
+    } else {
+        isDark = true;
+        setCookie('darkMode', 'true', 7);
+        // console.log(document.cookie);
+        body.classList.add('dark');
+    }
+}
+
+// function applyTheme() {
+//     let cookies = document.cookie.split("darkMode=");
+//     let darkModeState = cookies[cookies.length - 1];
+//     if (darkModeState === "true") {
+//         isDark = true;
+//     } else {
+//         isDark = false;
+//     }
+//     isDark = !isDark;
+//     changeTheme();
+//     // console.log(darkModeState);
+//     // console.log("hello !");
+// }
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+let cookies = document.cookie.split("darkMode=");
+let darkModeState = cookies[cookies.length - 1];
+if (darkModeState === "true") {
+    isDark = true;
+} else {
+    isDark = false;
+}
+isDark = !isDark;
+changeTheme();
