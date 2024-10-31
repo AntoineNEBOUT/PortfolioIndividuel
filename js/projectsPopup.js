0e465325378cf682e6298d1d16da344eb0d5ef4c
Antoine NEBOUT <Antoine.NEBOUT@protonmail.com>
2024-10-31 15:35:04 +0100

    Upd:index.html and projectsPopup.js

--- js/projectsPopup.js
+++ js/projectsPopup.js
@@ -8,11 +8,12 @@ const popupProjects = document.getElementById('projects_popup');
 /* Initialisation des écouteurs d'événements liés à l'ouverture de la popup détaillant les projets */
 // Appelle la fonction 'openPopup' lorsque la div du projet du portfolio personnel est cliquée
 openPortfolio.addEventListener('click', () => {
-    if (document.getElementById('project_title').innerHTML.trim() != 'Portfolio personnel') {
-        openPopup('portfolio');
-    } else {
-        openPopup('none');
-    }
+    // if (document.getElementById('project_title').innerHTML.trim() != 'Portfolio personnel') {
+    //     openPopup('portfolio');
+    // } else {
+    //     openPopup('none');
+    // }
+    openPopup('portfolio');
 });
 
 // Appelle la fonction 'openPopup' lorsque la div du projet du site wordpress est cliquée
@@ -37,21 +38,49 @@ function openPopup(projectName) {
     */
     switch (projectName) {
         case 'portfolio':
-			document.getElementById('project_title').innerHTML = "Portfolio personnel";
-			document.getElementById('project_description').innerHTML = "Il s’agit d’un projet dont l’objectif est d’aboutir à la création d’un portfolio personnel. Il m’a permis de mettre en pratique mes compétences organisationnelles et de design avec Figma. Ce projet s’inscrit dans un projet plus important, celui de réaliser à terme : un portfolio commun pour notre promotion.";
-			document.getElementById('project_image').src = 'images\\projectsPictures\\portfolioLogo.avif';
+			// document.getElementById('project_title').innerHTML = "Portfolio personnel";
+			// document.getElementById('project_description').innerHTML = "Il s’agit d’un projet dont l’objectif est d’aboutir à la création d’un portfolio personnel. Il m’a permis de mettre en pratique mes compétences organisationnelles et de design avec Figma. Ce projet s’inscrit dans un projet plus important, celui de réaliser à terme : un portfolio commun pour notre promotion.";
+			// document.getElementById('project_image').src = 'images\\projectsPictures\\portfolioLogo.avif';
+            document.getElementById('project_title_portfolio').classList.remove("hidden");
+            document.getElementById('project_description_portfolio').classList.remove("hidden");
+            document.getElementById('project_image_portfolio').classList.remove("hidden");
+            document.getElementById('project_title_esport').classList.add("hidden");
+            document.getElementById('project_description_esport').classList.add("hidden");
+            document.getElementById('project_image_esport').classList.add("hidden");
+            document.getElementById('project_title_nsi').classList.add("hidden");
+            document.getElementById('project_description_nsi').classList.add("hidden");
+            document.getElementById('project_image_nsi').classList.add("hidden");
             break;
 
         case 'esport':
-			document.getElementById('project_title').innerHTML = "Site vitrine pour une équipe d'esport";
-			document.getElementById('project_description').innerHTML = "Il s’agit d’un projet visant à réaliser un site web en utilisant WordPress. Cela comprend la rédaction de différents documents tels qu’un cahier des charges et des spécifications fonctionnelles. Ce projet complet nous a permis de mettre en pratique à la fois nos compétences en gestion de projet et en développement.";
-			document.getElementById('project_image').src = 'images\\projectsPictures\\g4EsportLyonLogo.png';
+			// document.getElementById('project_title').innerHTML = "Site vitrine pour une équipe d'esport";
+			// document.getElementById('project_description').innerHTML = "Il s’agit d’un projet visant à réaliser un site web en utilisant WordPress. Cela comprend la rédaction de différents documents tels qu’un cahier des charges et des spécifications fonctionnelles. Ce projet complet nous a permis de mettre en pratique à la fois nos compétences en gestion de projet et en développement.";
+			// document.getElementById('project_image').src = 'images\\projectsPictures\\g4EsportLyonLogo.png';
+            document.getElementById('project_title_portfolio').classList.add("hidden");
+            document.getElementById('project_description_portfolio').classList.add("hidden");
+            document.getElementById('project_image_portfolio').classList.add("hidden");
+            document.getElementById('project_title_esport').classList.remove("hidden");
+            document.getElementById('project_description_esport').classList.remove("hidden");
+            document.getElementById('project_image_esport').classList.remove("hidden");
+            document.getElementById('project_title_nsi').classList.add("hidden");
+            document.getElementById('project_description_nsi').classList.add("hidden");
+            document.getElementById('project_image_nsi').classList.add("hidden");
             break;
 
         case 'nsi':
-			document.getElementById('project_title').innerHTML = "Gestionnaire de tournoi - Les Trophées NSI 2024";
-			document.getElementById('project_description').innerHTML = "Il s'agit d'un projet de logiciel collaboratif et communautaire de gestion et de visualisation de tournois. C'est une application de bureau développée avec Python associé à PyQt5. Ce projet a eu lieu dans le cadre du concours ”Les Trophées NSI” (2024).";
-			document.getElementById('project_image').src = 'images\\projectsPictures\\olympicTournamentImage.avif';
+			// document.getElementById('project_title').innerHTML = "Gestionnaire de tournoi - Les Trophées NSI 2024";
+			// document.getElementById('project_description').innerHTML = "Il s'agit d'un projet de logiciel collaboratif et communautaire de gestion et de visualisation de tournois. C'est une application de bureau développée avec Python associé à PyQt5. Ce projet a eu lieu dans le cadre du concours ”Les Trophées NSI” (2024).";
+			// document.getElementById('project_image').src = 'images\\projectsPictures\\olympicTournamentImage.avif';
+            
+            document.getElementById('project_title_portfolio').classList.add("hidden");
+            document.getElementById('project_description_portfolio').classList.add("hidden");
+            document.getElementById('project_image_portfolio').classList.add("hidden");
+            document.getElementById('project_title_esport').classList.add("hidden");
+            document.getElementById('project_description_esport').classList.add("hidden");
+            document.getElementById('project_image_esport').classList.add("hidden");
+            document.getElementById('project_title_nsi').classList.remove("hidden");
+            document.getElementById('project_description_nsi').classList.remove("hidden");
+            document.getElementById('project_image_nsi').classList.remove("hidden");
             break;
 
         default:
